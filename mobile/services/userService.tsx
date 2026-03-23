5bbf696887f5a9d57a737db93ea4bee983e0c0b0
PanidaSukumpung <panidasukumpung@gmail.com>
2026-03-23 22:11:36 +0700

    add

--- mobile/services/userService.tsx
+++ mobile/services/userService.tsx
@@ -2,12 +2,28 @@ import axios from "axios";
 
 const API_URL = "http://localhost:8080/auth";
 
+export type Address = {
+  address_id: string;
+  user_id: string;
+  label: string;
+  receiver_name: string;
+  district: string;
+  subDistrict: string;
+  province: string;
+  postal_code: string;
+  details: string | null;
+  lat: number | null;
+  lng: number | null;
+  phone: string;
+};
+
 export type User = {
   user_id: string;
   email: string;
   name: string;
   phone: string;
   role: string;
+  addresses: Address[];
 };
 
 
